690c06b8e1a3e1cc5d82bb4c21f70c26ab992df1
solarbear <sons1998@naver.com>
2023-12-12 17:10:34 +0900

    상품 도메인 비즈니스 로직 추가

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,5 +25,18 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+
+    //비즈니스 로직 추가
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int resStock = this.stockQuantity - quantity;
+
+        if(resStock < 0) throw new NotEnoughStockException("수량이 없습니다");
+        this.stockQuantity = resStock;
+    }
 }
 
