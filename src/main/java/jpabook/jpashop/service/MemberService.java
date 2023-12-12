9da3fdb23f03620b687f9fe4c121a44abe35ba1d
solarbear <sons1998@naver.com>
2023-12-12 15:56:59 +0900

    회원 도메인

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,42 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> memberList = memberRepository.findByName(member.getName());
+        if(!memberList.isEmpty()) {
+            throw new IllegalStateException("이미 가입된 회원입니다.");
+        }
+    }
+
+    public Member findOne(Long id) {
+        return memberRepository.findOne(id);
+    }
+
+    public List<Member> findAll() {
+        return memberRepository.findAll();
+    }
+}
