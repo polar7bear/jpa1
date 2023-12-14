17cb36ec31376c1bdf909eb40a3208f61ad8252b
solarbear <sons1998@naver.com>
2023-12-14 17:43:32 +0900

    상품 목록, 수정

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -1,13 +1,15 @@
 package jpabook.jpashop.controller;
 
 import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.dto.BookForm;
 import jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -35,5 +37,43 @@ public class ItemController {
         return "redirect:/items";
     }
 
+    @GetMapping("/items")
+    public String findItems(Model model) {
+        List<Item> itemList = itemService.findAll();
+        model.addAttribute("items", itemList);
+
+        return "items/itemList";
+    }
+
+    @GetMapping("/items/{itemId}/edit")
+    public String editItem(@PathVariable("itemId") Long itemId, Model model) {
+        Book book = new Book();
+        Book item = (Book) itemService.findOne(itemId);
+        book.setId(item.getId());
+        book.setName(item.getName());
+        book.setPrice(item.getPrice());
+        book.setStockQuantity(item.getStockQuantity());
+        book.setAuthor(item.getAuthor());
+        book.setIsbn(item.getIsbn());
+
+        model.addAttribute("form", book);
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("/items/{itemId}/edit")
+    public String editItem(@ModelAttribute("form") BookForm form, Model model) {
+        Book book = new Book();
+
+        book.setId(form.getId());
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.addItem(book);
+
+        return "redirect:/items";
+    }
 
 }
